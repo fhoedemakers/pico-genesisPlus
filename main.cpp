cac37e94b6fb97731739fd6f4261d653427de6cd
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2024-12-19 14:14:55 +0100

    mulriple chnageschanges

---
+++ main.cpp
@@ -0,0 +1,403 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/divider.h"
+#include "hardware/clocks.h"
+#include "hardware/vreg.h"
+#include "hardware/watchdog.h"
+#include "util/work_meter.h"
+#include "ff.h"
+#include "tusb.h"
+#include "gamepad.h"
+#include "menu.h"
+#include "nespad.h"
+#include "wiipad.h"
+#include "FrensHelpers.h"
+#include "settings.h"
+#include "FrensFonts.h"
+
+#include "shared.h"
+#include "mytypes.h"
+
+
+bool isFatalError = false;
+static FATFS fs;
+char *romName;
+
+static bool fps_enabled = false;
+static uint32_t start_tick_us = 0;
+static uint32_t fps = 0;
+static char fpsString[3] = "00";
+#define fpsfgcolor 0;     // black
+#define fpsbgcolor 0xFFF; // white
+
+#define MARGINTOP 24
+#define MARGINBOTTOM 4
+
+#define FPSSTART (((MARGINTOP + 7) / 8) * 8)
+#define FPSEND ((FPSSTART) + 8)
+
+bool reset = false;
+
+
+namespace
+{
+    constexpr uint32_t CPUFreqKHz = 252000;
+}
+
+int sampleIndex = 0;
+void in_ram(processaudio)(int offset)
+{
+    int samples = 4; // 735/192 = 3.828125 192*4=768 735/3=245
+
+    if (offset == (IS_GG ? 24 : 0))
+    {
+        sampleIndex = 0;
+    }
+    else
+    {
+        sampleIndex += samples;
+        if (sampleIndex >= 735)
+        {
+            return;
+        }
+    }
+    short *p1 = snd.buffer[0] + sampleIndex;
+    short *p2 = snd.buffer[1] + sampleIndex;
+    while (samples)
+    {
+        auto &ring = dvi_->getAudioRingBuffer();
+        auto n = std::min<int>(samples, ring.getWritableSize());
+        if (!n)
+        {
+            return;
+        }
+        auto p = ring.getWritePointer();
+        int ct = n;
+        while (ct--)
+        {
+            int l = (*p1++ << 16) + *p2++;
+            // works also : int l = (*p1++ + *p2++) / 2;
+            int r = l;
+            // int l = *wave1++;
+            *p++ = {static_cast<short>(l), static_cast<short>(r)};
+        }
+        ring.advanceWritePointer(n);
+        samples -= n;
+    }
+}
+
+
+
+extern "C" void in_ram(sms_render_line)(int line, const uint8_t *buffer)
+{
+    // DVI top margin has #MARGINTOP lines
+    // DVI bottom margin has #MARGINBOTTOM lines
+    // DVI usable screen estate: MARGINTOP .. (240 - #MARGINBOTTOM)
+    // SMS has 192 lines
+    // GG  has 144 lines
+    // gg : Line starts at line 24
+    // sms: Line starts at line 0
+    // Emulator loops from scanline 0 to 261
+    // Audio needs to be processed per scanline
+
+    processaudio(line);
+    // Adjust line number to center the emulator display
+    line += MARGINTOP;
+    // Only render lines that are visible on the screen, keeping into account top and bottom margins
+    if (line < MARGINTOP || line >= 240 - MARGINBOTTOM)
+        return;
+
+    auto b = dvi_->getLineBuffer();
+    uint16_t *sbuffer;
+    if (buffer)
+    {
+        uint16_t *sbuffer = b->data() + 32 + (IS_GG ? 48 : 0);
+        for (int i = screenCropX; i < BMP_WIDTH - screenCropX; i++)
+        {
+            sbuffer[i - screenCropX] = palette444[(buffer[i + BMP_X_OFFSET]) & 31];
+        }
+    }
+    else
+    {
+        sbuffer = b->data() + 32;
+        __builtin_memset(sbuffer, 0, 512);
+    }
+    // Display frame rate
+    if (fps_enabled && line >= FPSSTART && line < FPSEND)
+    {
+        WORD *fpsBuffer = b->data() + 40;
+        int rowInChar = line % 8;
+        for (auto i = 0; i < 2; i++)
+        {
+            char firstFpsDigit = fpsString[i];
+            char fontSlice = getcharslicefrom8x8font(firstFpsDigit, rowInChar);
+            for (auto bit = 0; bit < 8; bit++)
+            {
+                if (fontSlice & 1)
+                {
+                    *fpsBuffer++ = fpsfgcolor;
+                }
+                else
+                {
+                    *fpsBuffer++ = fpsbgcolor;
+                }
+                fontSlice >>= 1;
+            }
+        }
+    }
+    dvi_->setLineBuffer(line, b);
+}
+
+
+int ProcessAfterFrameIsRendered()
+{
+#if NES_PIN_CLK != -1
+    nespad_read_start();
+#endif
+    auto count = dvi_->getFrameCounter();
+    auto onOff = hw_divider_s32_quotient_inlined(count, 60) & 1;
+    Frens::blinkLed(onOff);
+#if NES_PIN_CLK != -1
+    nespad_read_finish(); // Sets global nespad_state var
+#endif
+    // nespad_read_finish(); // Sets global nespad_state var
+    tuh_task();
+    // Frame rate calculation
+    if (fps_enabled)
+    {
+        // calculate fps and round to nearest value (instead of truncating/floor)
+        uint32_t tick_us = Frens::time_us() - start_tick_us;
+        fps = (1000000 - 1) / tick_us + 1;
+        start_tick_us = Frens::time_us();
+        fpsString[0] = '0' + (fps / 10);
+        fpsString[1] = '0' + (fps % 10);
+    }
+    return count;
+}
+
+static DWORD prevButtons[2]{};
+static DWORD prevButtonssystem[2]{};
+static DWORD prevOtherButtons[2]{};
+
+#define OTHER_BUTTON1 (0b1)
+#define OTHER_BUTTON2 (0b10)
+
+#define NESPAD_SELECT (0x04)
+#define NESPAD_START (0x08)
+#define NESPAD_UP (0x10)
+#define NESPAD_DOWN (0x20)
+#define NESPAD_LEFT (0x40)
+#define NESPAD_RIGHT (0x80)
+#define NESPAD_A (0x01)
+#define NESPAD_B (0x02)
+
+static int rapidFireMask[2]{};
+static int rapidFireCounter = 0;
+void processinput(DWORD *pdwPad1, DWORD *pdwPad2, DWORD *pdwSystem, bool ignorepushed, char *gamepadType)
+{
+    // pwdPad1 and pwdPad2 are only used in menu and are only set on first push
+    *pdwPad1 = *pdwPad2 = *pdwSystem = 0;
+
+    int smssystem[2]{};
+    unsigned long pushed, pushedsystem, pushedother;
+    bool usbConnected = false;
+    for (int i = 0; i < 2; i++)
+    {
+        int nespadbuttons = 0;
+        auto &dst = (i == 0) ? *pdwPad1 : *pdwPad2;
+        auto &gp = io::getCurrentGamePadState(i);
+        if ( i == 0 )
+        {
+            usbConnected = gp.isConnected();
+            if (gamepadType) {
+                strcpy(gamepadType, gp.GamePadName);
+            }
+        }
+      
+
+#if NES_PIN_CLK != -1
+        // When USB controller is connected both NES ports act as controller 2
+        if (usbConnected)
+        {          
+            if (i == 1)
+            {
+                 nespadbuttons= nespadbuttons | nespad_states[1] | nespad_states[0];
+            }
+        }
+        else
+        {
+            nespadbuttons |= nespad_states[i];
+        }
+#endif
+// When USB controller is connected  wiipad acts as controller 2 
+#if WII_PIN_SDA >= 0 and WII_PIN_SCL >= 0
+        if (usbConnected)
+        {
+            if (i == 1)
+            {
+                nespadbuttons |= wiipad_read();
+            }
+        }
+        else // if no USB controller is connected, wiipad acts as controller 1
+        {
+            if (i == 0)
+            {
+                nespadbuttons |= wiipad_read();
+            }
+        }
+#endif
+       
+
+        // if (gp.buttons & io::GamePadState::Button::SELECT) printf("SELECT\n");
+        // if (gp.buttons & io::GamePadState::Button::START) printf("START\n");
+        input.pad[i] = smsbuttons;
+        auto p1 = smssystem[i];
+        if (ignorepushed == false)
+        {
+            pushed = smsbuttons & ~prevButtons[i];
+            pushedsystem = smssystem[i] & ~prevButtonssystem[i];
+            pushedother = otherButtons & ~prevOtherButtons[i];
+        }
+        else
+        {
+            pushed = smsbuttons;
+            pushedsystem = smssystem[i];
+            pushedother = otherButtons;
+        }
+        if (p1 & INPUT_PAUSE)
+        {
+            if (pushedsystem & INPUT_START)
+            {
+                reset = true;
+                printf("Reset pressed\n");
+            }
+        }
+        if (p1 & INPUT_START)
+        {
+            // Toggle frame rate display
+            if (pushed & INPUT_BUTTON1)
+            {
+                fps_enabled = !fps_enabled;
+                printf("FPS: %s\n", fps_enabled ? "ON" : "OFF");
+            }
+            if (pushed & INPUT_UP)
+            {
+                Frens::screenMode(-1);
+            }
+            else if (pushed & INPUT_DOWN)
+            {
+                Frens::screenMode(+1);
+            }
+        }
+        prevButtons[i] = smsbuttons;
+      
+        // return only on first push
+        if (pushed)
+        {
+            dst = smsbuttons;
+        }
+        if (pushedother)
+        {
+            if (pushedother & OTHER_BUTTON1)
+            {
+                printf("Other 1\n");
+            }
+            if (pushedother & OTHER_BUTTON2)
+            {
+                printf("Other 2\n");
+            }
+        }
+    }
+}
+
+void in_ram(process)(void)
+{
+    DWORD pdwPad1, pdwPad2, pdwSystem; // have only meaning in menu
+    while (reset == false)
+    {
+        processinput(&pdwPad1, &pdwPad2, &pdwSystem, false, nullptr);
+        // renderframe
+        // TODO
+        ProcessAfterFrameIsRendered();
+    }
+}
+
+/// @brief
+/// Start emulator. Emulator does not run well in DEBUG mode, lots of red screen flicker. In order to keep it running fast enough, we need to run it in release mode or in
+/// RelWithDebugInfo mode.
+/// @return
+int main()
+{
+    char selectedRom[FF_MAX_LFN];
+    romName = selectedRom;
+    ErrorMessage[0] = selectedRom[0] = 0;
+   
+    int fileSize = 0;
+    bool isGameGear = false;
+
+    // Set voltage and clock frequency
+    vreg_set_voltage(VREG_VOLTAGE_1_20);
+    sleep_ms(10);
+    set_sys_clock_khz(CPUFreqKHz, true);
+
+    stdio_init_all();
+    sleep_ms(500);
+    printf("Starting Master System Emulator\n");
+    printf("CPU freq: %d\n", clock_get_hz(clk_sys));
+    printf("Starting Tinyusb subsystem\n");
+    tusb_init();
+    isFatalError =  !Frens::initAll(selectedRom, CPUFreqKHz, MARGINTOP, MARGINBOTTOM );
+    bool showSplash = true;
+
+    while (true)
+    {
+        if (strlen(selectedRom) == 0 || reset == true)
+        {
+            // reset margin to give menu more screen space
+            dvi_->getBlankSettings().top = 4 * 2;
+            dvi_->getBlankSettings().bottom = 4 * 2;
+            scaleMode8_7_ = Frens::applyScreenMode(ScreenMode::NOSCANLINE_8_7);
+            menu("Pico-SMS+", ErrorMessage, isFatalError, showSplash, ".sms .gg"); // never returns, but reboots upon selecting a game
+        }
+        reset = false;
+        FRESULT fr;
+        FIL file;
+        fr = f_open(&file, selectedRom, FA_READ);
+        if (fr != FR_OK)
+        {
+            snprintf(ErrorMessage, 40, "Cannot open rom %d", fr);
+            printf("%s\n", ErrorMessage);
+            selectedRom[0] = 0;
+            continue;
+        }
+        fileSize = f_size(&file);
+        f_close(&file);
+        isGameGear = Frens::cstr_endswith(selectedRom, ".gg");
+        printf("Now playing: %s (%d bytes)\n", selectedRom, fileSize);
+        if (isGameGear)
+        {
+            printf("Game Gear rom detected\n");
+        } else {
+            printf("Master System rom detected\n");
+        }
+        load_rom(ROM_FILE_ADDR, fileSize, isGameGear);
+        // Initialize all systems and power on
+        system_init(SMS_AUD_RATE);
+        // load state if any
+        // system_load_state();
+        system_reset();
+        printf("Starting game\n");
+        process();
+        system_shutdown();
+        selectedRom[0] = 0;
+        showSplash = false;
+    }
+
+    return 0;
+}
