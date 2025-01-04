d655561e7032bf72050e0ff612fb0d85f39ce0ad
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2025-01-04 11:48:59 +0100

    Comment out SCREEN pointer initialization in buffers.c

--- gwenesis/buffers.c
+++ gwenesis/buffers.c
@@ -8,7 +8,7 @@ unsigned char *M68K_RAM;
 unsigned char *ZRAM; // [MAX_Z80_RAM_SIZE];
 unsigned char *VRAM; // [VRAM_MAX_SIZE];
 // Declare SCREEN as a pointer to an array of 320 uint8_t elements
-uint8_t (*SCREEN)[320];
+// uint8_t (*SCREEN)[320];
 int16_t *gwenesis_sn76489_buffer; // [GWENESIS_AUDIO_BUFFER_LENGTH_NTSC * 2];  // 888 = NTSC, PAL = 1056 (too big) //GWENESIS_AUDIO_BUFFER_LENGTH_PAL];
 
 void init_emulator_mem() {
@@ -16,7 +16,7 @@ void init_emulator_mem() {
     ZRAM = (unsigned char *)malloc(MAX_Z80_RAM_SIZE);
     VRAM = (unsigned char *)malloc(VRAM_MAX_SIZE);
     // SCREEN = (uint8_t (*)[320])malloc(240 * 320 * sizeof(uint8_t));
-    SCREEN = NULL;
+    //SCREEN = NULL;
     gwenesis_sn76489_buffer = (int16_t *)malloc(GWENESIS_AUDIO_BUFFER_LENGTH_NTSC * 2 * sizeof(int16_t));
     // init all buffers
     memset(M68K_RAM, 0, MAX_RAM_SIZE);
