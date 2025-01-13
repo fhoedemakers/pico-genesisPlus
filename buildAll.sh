6e4a45ace6081116c3e624c176e654c337fc5967
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2025-01-13 09:29:47 +0100

    Add build script for Pico-genesisPlus emulator

---
+++ buildAll.sh
@@ -0,0 +1,42 @@
+:
+# ====================================================================================
+# Pico-genesisPlus build all script 
+# Builds the emulator for the default and alternate configuration.
+# Only RP2350 (Pico 2) supported.
+# Binaries are copied to the releases folder
+#   - pico2_picogenesisPlusPimoroniDV.uf2     Pimoroni Pico DV Demo Base
+#   - pico2_picogenesisPlusAdaFruitDVISD.uf2  AdaFruit HDMI and SD Breakout boards
+# ====================================================================================
+cd `dirname $0` || exit 1
+[ -d releases ] && rm -rf releases
+mkdir releases || exit 1
+# check picotool exists in path
+if ! command -v picotool &> /dev/null
+then
+	echo "picotool could not be found"
+	echo "Please install picotool from https://github.com/raspberrypi/picotool.git" 
+	exit
+fi
+# build for Pico is disabled. RP2040 builds are too slow.
+# HWCONFIGS="1 2 3 4"
+# for HWCONFIG in $HWCONFIGS
+# do	
+# 	./bld.sh -c $HWCONFIG
+# done
+# build for Pico 2 arm and risc-v
+HWCONFIGS="1 2"
+for HWCONFIG in $HWCONFIGS
+do
+	./bld.sh -c $HWCONFIG -2
+	#./bld.sh -c $HWCONFIG -r
+done
+if [ -z "$(ls -A releases)" ]; then
+	echo "No UF2 files found in releases folder"
+	exit
+fi
+for UF2 in releases/*.uf2
+do
+	ls -l $UF2
+	picotool info $UF2
+	echo " "
+done
